6b6e05c79433dbb88aa2d57ccea01676d95399d5
slim-gears <denis.itskovich@gmail.com>
2015-11-20 22:38:13 +0200

    Adding gradle-aio usage

--- slimbus-example/src/test/java/com/slimgears/slimbus/example/ApplicationTest.java
+++ slimbus-example/src/test/java/com/slimgears/slimbus/example/ApplicationTest.java
@@ -5,16 +5,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowToast;
 import org.robolectric.util.ActivityController;
 
-@RunWith(RobolectricTestRunner.class)
-@Config(sdk = 16,
-        manifest = "build/intermediates/manifests/full/" + BuildConfig.BUILD_TYPE + "/AndroidManifest.xml",
-        resourceDir = "../../../res/" + BuildConfig.BUILD_TYPE)
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(sdk = 16, constants = BuildConfig.class)
 public class ApplicationTest {
     private ActivityController<MainActivity> mainActivityController;
     private App app;
